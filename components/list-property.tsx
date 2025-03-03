74966032356c5609d71a9aa2ad623c8a33dd0b95
issa-bourasse <141015850+issa-bourasse@users.noreply.github.com>
2025-03-03 13:33:07 +0100

    Add explicit type for the `e` parameter in the `handleChange` function
    * Add `React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>` type to `e` parameter in `handleChange`
    * Add `React.FormEvent<HTMLFormElement>` type to `e` parameter in `handleSubmit`

--- components/list-property.tsx
+++ components/list-property.tsx
@@ -17,7 +17,7 @@ export default function ListProperty() {
     images: null,
   })
 
-  const handleChange = (e) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value, files } = e.target
     setFormData((prevState) => ({
       ...prevState,
@@ -25,7 +25,7 @@ export default function ListProperty() {
     }))
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     console.log("Form submitted:", formData)
     // Handle form submission logic here
@@ -127,4 +127,3 @@ export default function ListProperty() {
     </div>
   )
 }
-
